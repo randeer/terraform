703ff5d1cb1d632b33a11b6e3e7555e8475dd1ec
Ubuntu <binara@k8s-binara.xwclfomz1xjefaatcyqg0dad0e.jx.internal.cloudapp.net>
2023-02-01 05:28:30 +0000

    terraform-k8s

---
+++ k8s/k8s-main.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+     #version = "2.17.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+  # Configuration options
+    config_path = "~/.kube/config"
+}
+
+resource "kubernetes_namespace" "from-terraform" {
+  metadata {
+    name = "from-terraform"
+  }
+}
+
+resource "kubernetes_deployment" "test" {
+  metadata {
+    name      = "randeer-cv"
+    namespace = kubernetes_namespace.from-terraform.metadata.0.name
+  }
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        app = "MyTestApp"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "MyTestApp"
+        }
+      }
+      spec {
+        container {
+          image = "randeerdil/cv_template:58"
+          name  = "nginx-container"
+          port {
+            container_port = 443
+          }
+        }
+      }
+    }
+  }
+}
+
+
+output "k8s-image-name" {
+  value = kubernetes_deployment.test.spec.0.template.0.spec.0.container.0.image
+}

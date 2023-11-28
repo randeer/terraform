7075cf362874ab00d7cf2a21ef3dec133c8fae70
Randeer Lalanga <39120946+randeer@users.noreply.github.com>
2023-11-28 18:30:13 +0530

    Create myapp.tf

---
+++ k8s/myapp.tf
@@ -0,0 +1,43 @@
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "my-context"
+}
+
+variable "namespace_name" {}
+variable "resource_name" {}
+variable "issuer_name" {}
+variable "email" {}
+variable "private_key_secret_ref" {}
+
+resource "kubernetes_namespace" var.resource_name {
+  metadata {
+    name = var.namespace_name
+  }
+}
+
+
+resource "cert-manager_issuer" var.resource_name {
+  apiVersion = "cert-manager.io/v1"
+  kind       = "Issuer"
+  metadata {
+    name      = var.issuer_name
+    namespace = var.namespace_name
+  }
+  spec {
+    acme {
+      server             = https://acme-v02.api.letsencrypt.org/directory
+      email              = var.email
+      privateKeySecretRef {
+        name = var.private_key_secret_ref
+      }
+      solvers {
+        selector = {}
+        http01 {
+          ingress {
+            ingressClassName = nginx
+          }
+        }
+      }
+    }
+  }
+}

140aa3676f43c6b386fec84586b027c69c29ef2e
Randeer Lalanga <39120946+randeer@users.noreply.github.com>
2023-11-28 18:55:01 +0530

    Update myapp.tf

--- k8s/myapp.tf
+++ k8s/myapp.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_version = ">= 0.13"
+
+  required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.0"
+    }
+  }
+}
+
 provider "kubernetes" {
   config_path    = "~/.kube/config"
   config_context = "my-context"
